57929032beccefebb7dad9791d12ed73055a2e10
Serhat Ataman <serhat.ataman@sysco.no>
2018-10-08 13:39:51 +0200

    initial commit

--- src/main/java/router/MyRouteBuilder.java
+++ src/main/java/router/MyRouteBuilder.java
@@ -9,6 +9,12 @@ public class MyRouteBuilder extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
+        /*
+        * There is a text file in input file. Take it and process it.
+        * In process, take the string which in text file and write it to terminal.
+        * In Bean(where we do data transformation), take the data as String and transform it to UpperCase
+        * Finally, COPY file to output directory.
+        * */
         from("file:/tmp/input?move=./done")
                 .process(new MyLogProcessor())
                 .bean(new MyTransformer(), "transformContent")
